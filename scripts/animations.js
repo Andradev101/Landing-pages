35ac830e614a46f255acee04f3b6e64f2b8a8157
Andradev101 <contato.santosti@gmail.com>
2021-02-03 10:43:31 -0300

    Minor styling changes, animations, javascript minor updates

--- scripts/animations.js
+++ scripts/animations.js
@@ -2,6 +2,7 @@ var hidebtn = document.getElementById("hidebtn");
 var showbtn = document.getElementById("showbtn");
 var headerbtn = document.getElementsByTagName("header");
 var maincontent = document.getElementsByClassName("main-content");
+var products = document.querySelectorAll("div.products");
 
 hidebtn.addEventListener("click", clickhide);
 showbtn.addEventListener("click", clickshow);
@@ -17,4 +18,12 @@ function clickshow(){
     headerbtn[0].style.transform="translateX(0px)";
     maincontent[0].style.marginLeft="200px";
     showbtn.style.opacity="0"; 
+}
+
+//each product.div has +0.2 delay from the same behind
+    for (var i = 0; i < products.length; ++i) {
+    var item = products[i];
+    var delay = i*0.2;
+    products[i].style.animationDelay=delay+"s"
+    console.log(products[i])
 }
