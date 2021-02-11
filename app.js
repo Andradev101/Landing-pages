506b4809107487a804c320fe61db8540db5fb3cd
Andradev101 <contato.santosti@gmail.com>
2021-02-11 12:55:59 -0300

    Refatoring project for  express

--- app.js
+++ app.js
@@ -2,13 +2,27 @@ const express = require('express');
 const app = express();
 const port = 8081;
 
+//css
 app.use(express.static(__dirname+'/public'));
 
-
+//paths
 app.get('/', (req, res) =>{
-    res.sendFile(__dirname+'/public/index.html')
+    res.sendFile(__dirname+'/public/views/index.html')
+});
+app.get('/products', (req, res) =>{
+    res.sendFile(__dirname+'/public/views/products.html')
+});
+app.get('/services', (req, res) =>{
+    res.sendFile(__dirname+'/public/views/services.html')
+});
+app.get('/contact', (req, res) =>{
+    res.sendFile(__dirname+'/public/views/contact.html')
+});
+app.get('/about', (req, res) =>{
+    res.sendFile(__dirname+'/public/views/about.html')
 });
 
+//server listener
 app.listen(port,()=>{
 console.log(`servidor aberto em: http://localhost:${port}`);
 })
